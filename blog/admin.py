db91fa751a24134255c964ce6f202fbb92b3ace9
gustavodsantos <gustavojuniordos@hotmail.com>
2024-03-04 16:44:42 -0300

    meus arquivos

---
+++ blog/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Post
+
+
+@admin.register(Post)
+class Post(admin.ModelAdmin):
+    list_display = ('author', 'title', 'text', 'created_date', 'puplished_date')
